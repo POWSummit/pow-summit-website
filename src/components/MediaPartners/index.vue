abfdbcea08b45873090cc852ec5d024540ab58b7
Andrew <andrewgdick@gmail.com>
2023-08-21 13:04:55 -0400

    Update index.vue

--- src/components/MediaPartners/index.vue
+++ src/components/MediaPartners/index.vue
@@ -1,15 +1,23 @@
 <template>
-	<section class="mediaPartnersSection">
-		<h1>Media Sponsors</h1>
-		<div class="mediaPartnersContainer">
-			<a target="_blank" href="https://icoholder.com/">
-				<img src="@/assets/images/icoholder.png" alt="Icoholder" />
+	<section class="activeSponsorsSection" id="activeSponsorsSection">
+		<h1>Sponsors</h1>
+		<div class="activeSponsorsContainer">
+			<a target="_blank" href="https://etccooperative.org/">
+				<img src="@/assets/images/etc-coop.png" alt="ETC Cooperative" />
 			</a>
-			<a target="_blank" href="https://www.cryptonewsz.com/">
-				<img src="@/assets/images/cryptonewsz.png" alt="CryptoNewsZ" />
+            <a target="_blank" href="https://m.jasminer.com/">
+				<img src="@/assets/images/JASMINER_logo.png" alt="Jasminer" />
 			</a>
-			<a target="_blank" href="https://events.coinpedia.org/">
-				<img src="@/assets/images/coinpedia.png" alt="Coinpedia" />
+
+			<a target="_blank" href="https://lumerin.io/">
+				<img src="@/assets/images/lumerin.png" alt="Lumerin" />
+			</a>
+
+			<a target="_blank" href="https://hivedigitaltechnologies.com/">
+				<img
+					src="@/assets/images/hive-digital.png"
+					alt="HIVE Digital Technologies"
+				/>
 			</a>
 		</div>
 	</section>
