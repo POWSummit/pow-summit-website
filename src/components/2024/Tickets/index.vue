4bf393cda92d431d4b738c60ea49be3f75e1be17
Andrew <andrewgdick@gmail.com>
2024-09-05 08:44:31 -0400

    update AA PP section (#286)
    * Add files via upload
    
    * Update speakers.json updated speakers
    
    * Update speakers.json
    
    * Update speakers.json
    
    * Delete public/img/speakers/⁨MatthiasTarasiewicz⁩.jpg
    
    * Add files via upload
    
    * Add files via upload
    
    * Rename ChrisSeedor.png to ChrisSeedor.jpg
    
    * Update speakers.json
    
    * Update speakers.json updated Donalds Twitter
    
    * Update speakers.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Update speakers.json
    
    * add coin telegraph image
    
    * CT image
    
    * Update index.vue
    
    * Add files via upload
    
    * add hive logo
    
    * Update index.vue
    
    * Update index.vue added other events
    
    * add parapolis image
    
    * Update index.vue updated events PP
    
    * Add files via upload
    
    * Update styles.css update pp aa
    
    * updat aa pp graphic
    
    * Update styles.css
    
    * Update styles.css

--- src/components/2024/Tickets/index.vue
+++ src/components/2024/Tickets/index.vue
@@ -43,7 +43,32 @@
 			</a>
 		</div>
 	</section>
-
+<section class="assocatedEventsSection">
+		<div class="proofMainContainer assocatedEventsContainer">
+			<div class="title">
+				<h1>ASSOCIATED EVENTS</h1>
+				<h4>Hackers Congress Paralelní Polis</h4>
+				<h5>Oct. 4th – 6th, 2024</h5>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 pe-lg-4 pe-3 order-lg-1 order-2">
+					<a href="https://tix.hcpp.cz/24/redeem?voucher=POW24" target="_blank">
+						<img src="@/assets/images/hcpp-napis-na-2024.png" alt="" />
+					</a>
+				</div>
+				<div class="col-lg-6 mb-lg-0 mb-5 order-lg-2 order-1 text-center">
+					<p>
+						Attended by over 500 fans &amp; IT experts from around the 
+						world since 2014, HCPP is the flagship event of Paralelní
+						Polis.
+					</p>
+					<p>
+						Hackers Congress Paralelní Polis is a 3-day international conference on freedom, cryptoanarchy, decentralized finances, hacking and tech | October 4-6, 2024
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 <script>
 import ticketsConfig from "@/config/2024/tickets.json";
