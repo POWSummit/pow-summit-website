4770a49e5a2a15ec60c1a89abe06e5922a0aa9db
dchek <dimitrychekov@gmail.com>
2024-09-10 09:13:29 -0400

    Add new page and route for 2024 schedule.

---
+++ src/views/2024/Schedule.vue
@@ -0,0 +1,74 @@
+<template>
+	<!-- hero section with navbar starts here -->
+	<section class="proofHeroSecion">
+		<nav class="navbar navbar-expand-lg proofNav">
+			<div class="container-fluid">
+				<button
+					class="navbar-toggler order-lg-1 order-1"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div
+					class="collapse navbar-collapse order-lg-2 order-3"
+					id="navbarSupportedContent"
+				>
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="/"
+								>POW Summit</a
+							>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</section>
+	<section class="p-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-0 col-md-2 col-lg-2"></div>
+				<div class="col-sm-12 col-md-8 col-lg-8">
+					<h1 class="pb-4">2024 Schedule</h1>
+
+					<script type="text/javascript" src="https://pretalx.com/democon/schedule/widget/v2.en.js"></script>
+
+					<pretalx-schedule event-url="https://pretalx.com/democon/" locale="en" style="--pretalx-clr-primary: #3aa57c"></pretalx-schedule>
+					<noscript>
+					<div class="pretalx-widget">
+							<div class="pretalx-widget-info-message">
+								JavaScript is disabled in your browser. To access our schedule without JavaScript,
+								please <a target="_blank" href="https://pretalx.com/democon/schedule/">click here</a>.
+							</div>
+						</div>
+					</noscript>
+
+				</div>
+				<div class="col-sm-0 col-md-2 col-lg-2"></div>
+			</div>
+		</div>
+	</section>
+	<footer class="proofFooter">
+		<div class="footerContainer">
+			<ul class="footerLinks">
+				<li>
+					<a href="/">POW Summit</a>
+				</li>
+			</ul>
+			<h1>BROUGHT TO YOU BY</h1>
+			<div class="footerLogo">
+				<a href="https://litecoin.net">
+					<img src="@/assets/images/footer-logo1.png" alt="" />
+				</a>
+				<a href="https://etccooperative.org">
+					<img src="@/assets/images/footer-logo2.png" alt="" />
+				</a>
+			</div>
+		</div>
+	</footer>
+</template>
