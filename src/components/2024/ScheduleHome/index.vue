aa7e5f54419c2af24147eb45e0fd6182478e8593
dchek <dimitrychekov@gmail.com>
2024-09-16 22:11:44 -0400

    schedule page - add schedule section to home page

---
+++ src/components/2024/ScheduleHome/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="registerSpeakerSection" id="registerSpeakerSection">
+		<div class="proofMainContainer registerSpeakerContainer">
+			<a
+				href="./2024/schedule"
+				class="text-decoration-none"
+			>
+				<button>Event Schedule</button>
+			</a>
+		</div>
+	</section>
+</template>
+<script>
+import { getButton } from "@/config/2024/buttons.js";
+export default {
+	data() {
+		return {
+			becomeVolunteerButton: getButton("become_volunteer"),
+			becomeSponsorButton: getButton("become_sponsor"),
+		};
+	},
+};
+</script>
